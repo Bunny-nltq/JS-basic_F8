bdf9ef35a3bb58134398d22224a5e27b4839bf19
Bunny-nltq <bunny.nltq@gmail.com>
2025-08-07 16:32:25 +0700

    Làm việc với mảng

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-//Làm việc với kiểu số
+// 1. To String
+var languages = [
+    'Javascript',
+    'PHP',
+    'Ruby'
+];
 
-//Khai báo kiểu số
+// 2. Join
+console.log(languages.join(', ')) // chèn kí tự vào giữa các phần tử trong mảng
 
-var age = 20;
-var PI = 3.14
+// 3. Pop
+console.log(languages.pop()) //Xóa element cuối bảng và trả về phần tử đã xóa
 
-//--  To String
-console.log(age.toString()) // Chuyển đổi số sang kiểu string 
-= var myString = PI.toString();
+// 4. Push
+console.log(languages.push('Dart')) //Thêm 1 hoặc nhiều phần tử vào cuối mảng và trả về số phần tử trong mảng
 
-//--  To Fixed
-console.log(PI.toFixed()) // giá trị thập phân nhỏ hơn 0,5 hàm làm trong về giá trị dưới và ngược lại 
-console.log(PI.toFixed(2)) // làm tròn 2 số sau dấu chấm
+// 5. Shift
+console.log(languages.shift()) //Xóa phần tử đầu mảng và trả về phần tử đã xóa
+
+// 6. Unshift
+console.log(languages.shift()) //Thêm 1 hoặc nhiều phần tử vào đầu mảng và trả về số phần tử trong mảng
+
+// 7. Splicing
+languages.splice(1, 2, 'Dart')//đặt con trỏ ở phần tử số ..., từ vị trí đặt con trỏ xóa đi ... phần tử, truyền thêm phần tử tên '...' vào ngay vị trí con trỏ
+
+// 8. Concat
+var languages2 = [
+    'Dart',
+    'Ruby'
+]
+console.log(languages.concat(languages2)) //Gộp 2 mảng lại theo thứ tự trước sau
+
+// 9. Slicing
+console.log(languages.slice())//Cắt lấy element từ số ... trong mảng (còn lại bỏ)
