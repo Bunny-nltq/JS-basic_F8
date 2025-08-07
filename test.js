67a06efcb6ea7a029a436d76ef36870dd213f184
Bunny-nltq <bunny.nltq@gmail.com>
2025-08-07 09:32:35 +0700

    Bài 1 cơ bản

---
+++ test.js
@@ -0,0 +1,84 @@
+/*alert('Hi JavaScript basic!')
+
+//Khai bao bien
+var fullName = 'Nguyen Le Truc Quynh'
+var age = 20;
+
+alert(fullName)
+alert(age);
+*/
+/* 
+    noi dung o day la comment
+/**
+ * 
+ */
+
+/**  Console
+ * console.log(fullName)
+ * console.warm(fullName)
+ * console.error(fullName)
+*/
+
+/**confirm('Xac nhan ban du tuoi!) */
+/**prompt('Xac nhan ban du tuoi!) */
+
+/*
+setTimeout(function () {
+
+}, 1000)
+
+setInterval(function () {
+
+}, 1000)
+*/
+
+/** Toán tử */
+
+/**
+ * Toán tử số học 
+ * var a = 1 * 2
+ * console.log(a)
+ */
+
+/**
+ * Toán tử gán
+ * var fullName = 'Nguyen Le Truc Quynh'
+ */
+
+/** 
+ * Toán tử so sánh
+ * var a = 1; 
+ * var b = 2;
+ * 
+ * if(a > b) {
+ * // code 
+ * }
+ */
+
+/** 
+ * Toán tử logic
+ * var a = 1;
+ * var b = 2;
+ * if(a > 0 && b > 0){
+ *   //CODE
+ * }
+ */
+
+//Toán tử ++ --
+//Prefix & Postfix
+var a = 6
+
+//B1: +1 cho a, a = a + 1 => a = 7;
+//B2: Trả về a sau khi đc cộng 1
+var output = ++a;
+console.log('output: ', output)
+console.log('a: ', a)
+
+//B1: a copy, a copy = 6
+//B2: trừ 1  của a, a = a -1 => a = 5
+//B3: trả về a copy
+//var output = a++
+
+//console.log('output: ', output);
+
+//console.log('a: ', a)
