427406607fea59f77e32654862df248e65368cae
Bunny-nltq <bunny.nltq@gmail.com>
2025-08-07 15:54:09 +0700

    Làm việc với kiểu số

--- main.js
+++ main.js
@@ -1,39 +1,14 @@
-//LÀM VIỆC VỚI CHUỖI
+//Làm việc với kiểu số
 
-var myString = 'Hoc JS tai JS F8'
+//Khai báo kiểu số
 
-//Keyword: Javascript string methods
+var age = 20;
+var PI = 3.14
 
-//1.Length
-    console.log(myString.length)
+//--  To String
+console.log(age.toString()) // Chuyển đổi số sang kiểu string 
+= var myString = PI.toString();
 
-// 2.Find index
-    console.log(myString.indexOf('JS')) //tìm chỗi theo thứ tự JS đầu tiên trong string -1: không tìm thấy
-    console.log(myString.indexOf('JS', 6)) //tìm chữ JS tiếp theo sau thứ tự thứ 6
-    console.log(myString.lastIndexOf('JS')) //tìm chuỗi JS cuối cùng trong String
-
-// 3. Cut string
-    console.log(myString.slice(4,6)) //cắt chuỗi từ vị trí số ...- ... theo thứ tự từ trái sang phải
-    console.log(myString.slice(-3,-1)) //cắt chuỗi từ vị trí số ...- ... theo thứ tự từ phải sang trái
-
-//4. Replace 
-    console.log(myString.replace('JS', 'Javascript')) //Thay thế chứ JS đầu tiên bằng chữ 'Javascript'
-    console.log(myString.replace(/JS/g, 'Javascript')) //Tìm và Thay thế tất cả các chữ JS bằng chữ 'Javascript'
-
-//5. Convert to upper case
-    console.log(mystring.toUpperCase()) //chuyển tất cả thành chữ in hoa
-//6. Convert to lower case 
-    console.log(mystring.toLowerCase()) //chuyển tất cả thành chữ in thường
-
-//7. Trim
-    console.log(myString.trim()) //loại bỏ khoảng trắng ở 2 đầu
-
-//8. Split
-    var languages = 'JavaScript, PHP, Ruby'
-    console.log(languages.split(', ')) //cắt chuỗi bằng cách tìm điểm chung vd trong đoạn chuỗi là ", "
-
-//9. Get a character by index
-    const myString2 = 'Son Dang'
-    //lấy chữ ở kí tự thứ ... trong chuỗi
-    console.log(myString2.charAt(10))
-    console.log(myString2[0])
+//--  To Fixed
+console.log(PI.toFixed()) // giá trị thập phân nhỏ hơn 0,5 hàm làm trong về giá trị dưới và ngược lại 
+console.log(PI.toFixed(2)) // làm tròn 2 số sau dấu chấm
